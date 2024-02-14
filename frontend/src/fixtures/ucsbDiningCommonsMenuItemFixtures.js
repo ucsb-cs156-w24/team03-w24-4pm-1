7e5d84e1264e28396b17bc205db72511d9ee886a
Tianyi Yang <tianyi_yang@ucsb.edu>
2024-02-13 18:15:23 -0800

    Added fixtures - T. Yang

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+    oneDiningCommonsMenuItem: {
+        "id": 1,
+        "station": "dlg",
+        "name": "dlg",
+        "code": "dlg"
+    },
+    threeDiningCommonsMenuItems: [
+        {
+            "id": 1,
+            "station": "dlg",
+            "name": "dlg",
+            "code": "dlg"
+        },
+        {
+            "id": 2,
+            "station": "portola",
+            "name": "portola",
+            "code": "portola"
+        },
+        {
+            "id": 3,
+            "station": "ortega",
+            "name": "ortega",
+            "code": "ortega"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemsFixtures };
