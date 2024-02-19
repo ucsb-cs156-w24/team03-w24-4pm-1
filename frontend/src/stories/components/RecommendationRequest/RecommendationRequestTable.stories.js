8359bb4660eee139687fb8dfa62abc383edad5cf
aarnavkumar <arnavkumar@umail.ucsb.edu>
2024-02-19 14:35:37 -0800

    Name change in storybook

--- frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestTable.stories.js
@@ -24,13 +24,13 @@ Empty.args = {
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.args = {
-    requests: RecommendationRequestFixtures.threeRequests,
+    requests: RecommendationRequestFixtures.threeRecommendationRequests,
     currentUser: currentUserFixtures.userOnly,
 };
 
 export const ThreeItemsAdminUser = Template.bind({});
 ThreeItemsAdminUser.args = {
-    requests: RecommendationRequestFixtures.threeRequests,
+    requests: RecommendationRequestFixtures.threeRecommendationRequests,
     currentUser: currentUserFixtures.adminUser,
 }
 
