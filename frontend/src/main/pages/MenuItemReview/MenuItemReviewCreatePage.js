c50242b0e0a1599675d77c953b6b5efbdf2f49bb
jinanthony <92065303+jinanthony@users.noreply.github.com>
2024-02-19 19:29:29 -0800

    Update MenuItemReviewCreatePage.js

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewCreatePage.js
@@ -1,5 +1,5 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import MenuItemReviewForm from "main/components/MenuItemReviews/MenuItemReviewForm";
+import MenuItemReviewForm from "main/components/MenuItemReviews/menuItemReviewForm";
 import { Navigate } from 'react-router-dom'
 import { useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
