0e98d9c637b262e4f18f2ec0637e496baabdab4d
Alexander <alexanderthomas@ucsb.edu>
2024-02-21 04:54:15 -0800

    intial commit 2

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationEditPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationEditPage.js
@@ -1,12 +1,68 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import UCSBOrganizationForm from "main/components/UCSBOrganizations/UCSBOrganizationForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function UCSBOrganizationEditPage() {
+export default function UCSBOrganizationEditPage({storybook=false}) {
+  let { orgCode } = useParams();
 
-  // Stryker disable all : placeholder for future implementation
+  const { data: ucsbOrg, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/ucsborganization?orgCode=${orgCode}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/ucsborganization`,
+        params: {
+          orgCode
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (ucsbOrg) => ({
+    url: "/api/ucsborganization",
+    method: "PUT",
+    params: {
+      orgCode: ucsbOrg.orgCode,
+    },
+    data: {
+      orgTranslationShort: ucsbOrg.orgTranslationShort,
+      orgTranslation: ucsbOrg.orgTranslation,
+      inactive: ucsbOrg.inactive
+    }
+  });
+
+  const onSuccess = (ucsbOrg) => {
+    toast(`UCSBOrganization Updated - orgCode: ${ucsbOrg.orgCode} orgTranslation: ${ucsbOrg.orgTranslation}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/ucsborganization?orgCode=${orgCode}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/ucsborganization" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit UCSBOrganization</h1>
+        {
+          ucsbOrg && <UCSBOrganizationForm initialContents={ucsbOrg} submitAction={onSubmit} buttonLabel="Update" />
+        }
       </div>
     </BasicLayout>
   )
